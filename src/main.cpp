3d912ed0a17f859c493bbc7106fdc6ebee662bb8
bilalaniq <bilalaniq2@gmail.com>
2025-10-22 13:33:42 +0500

    opengl added

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,27 @@
-#include "app-window.h"
-#include <sodium.h>
-#include <nlohmann/json.hpp>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
-int main(int argc, char **argv)
-{
-    auto ui = AppWindow::create();
+int main() {
+    glfwInit();
+    GLFWwindow* win = glfwCreateWindow(400, 400, "OpenGL Test", nullptr, nullptr);
+    glfwMakeContextCurrent(win);
+    glewInit();
 
-    
-    ui->run();
+    while(!glfwWindowShouldClose(win)) {
+        glClearColor(0,0,0,1);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glPointSize(50.0f);      // Make the point big
+        glBegin(GL_POINTS);
+            glColor3f(1,1,1);
+            glVertex2f(0.0f, 0.0f); // Center
+        glEnd();
+
+        glfwSwapBuffers(win);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(win);
+    glfwTerminate();
     return 0;
 }
